43e0ff6bc94b2faa283bbc794299a59fb58a6443
developermong2 <developermong2@gmail.com>
2024-07-01 17:53:36 +0900

    Feat : Next.js blog 연습

--- src/config/const.ts
+++ src/config/const.ts
@@ -1,6 +1,6 @@
 export const baseDomain = 'https://www.d5br5.dev';
 
-export const blogName = '김도형 개발 블로그';
-export const blogDesc = '개발 지식과 경험을 공유합니다.';
+export const blogName = 'Mori blog';
+export const blogDesc = 'Next.js 블로그 연습.';
 
 export const blogThumbnailURL = `${baseDomain}/blog_thumbnail.png`;
