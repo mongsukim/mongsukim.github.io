87ddba44cb5bc4a9965fa9c995b5315732ce4e51
developermong2 <developermong2@gmail.com>
2024-07-08 11:52:14 +0900

    Fix : netlify 배포설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+
+module.exports = {
+    output: 'export',
+ }
+
 // Merge MDX config with Next.js config
 export default nextConfig;
